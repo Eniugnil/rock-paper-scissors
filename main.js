6918b5cc1c4ace9d9ffc4b15b5c530df76c2d80f
Eniugnil <138859684+Eniugnil@[200~users.noreply.github.com>
2023-08-14 01:35:32 -0700

    Make playRound function

--- main.js
+++ main.js
@@ -7,4 +7,12 @@ function getComputerChoice() {
     } else return "scissors";
 }
 
-
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors") {return "Rock beats Scissors"}   
+    else if (playerSelection === "rock" && computerSelection === "paper") {return "Rock loses to Paper"}
+    else if (playerSelection === "paper" && computerSelection === "rock") {return "Paper beats Rock"}
+    else if (playerSelection === "paper" && computerSelection === "scissors") {return "Paper loses to Scissors"}
+    else if (playerSelection === "scissors" && computerSelection === "paper") {return "Scissors beats Paper"}
+    else if (playerSelection === "scissors" && computerSelection === "rock") {return "Scissors loses to Rock"}
+    else if (playerSelection === computerSelection) {return "Tie"}
+}
