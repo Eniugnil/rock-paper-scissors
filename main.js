66221b543fe2bede11c9ed260b8b1b94edf09373
Eniugnil <138859684+Eniugnil@[200~users.noreply.github.com>
2023-08-14 03:07:56 -0700

    Add a game function
    Increments were also added to playRound to make the while loop to work.

--- main.js
+++ main.js
@@ -7,12 +7,44 @@ function getComputerChoice() {
     } else return "scissors";
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "scissors") {return "Rock beats Scissors"}   
-    else if (playerSelection === "rock" && computerSelection === "paper") {return "Rock loses to Paper"}
-    else if (playerSelection === "paper" && computerSelection === "rock") {return "Paper beats Rock"}
-    else if (playerSelection === "paper" && computerSelection === "scissors") {return "Paper loses to Scissors"}
-    else if (playerSelection === "scissors" && computerSelection === "paper") {return "Scissors beats Paper"}
-    else if (playerSelection === "scissors" && computerSelection === "rock") {return "Scissors loses to Rock"}
-    else if (playerSelection === computerSelection) {return "Tie"}
+
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    while (playerWins != 5) {
+    console.log(playRound(prompt().toLowerCase(), getComputerChoice()));
+ 
+    function playRound(playerSelection, computerSelection) {
+
+        if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerWins++;
+            return "Rock beats Scissors"}   
+
+        else if (playerSelection === "rock" && computerSelection === "paper") {
+            computerWins++;
+            return "Rock loses to Paper"}
+
+        else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerWins++;
+            return "Paper beats Rock"}
+
+        else if (playerSelection === "paper" && computerSelection === "scissors") {
+            computerWins++;
+            return "Paper loses to Scissors"}
+
+        else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerWins++;
+            return "Scissors beats Paper"}
+
+        else if (playerSelection === "scissors" && computerSelection === "rock") {
+            computerWins++;
+            return "Scissors loses to Rock"}
+
+        else if (playerSelection === computerSelection) {return "Tie"}
+    }
+}
+if (playerWins == 5) {
+    return "You Win";
+}
+else return "Lose";
 }
